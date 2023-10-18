d3bffa47ccf27884e1743e533a17a9bc2781e379
Stiv <strodriguezna@gmail.com>
2023-10-18 12:07:30 +0200

    Agregando clase controller y entity

---
+++ src/main/java/com/digital/chat/springback/controllers/ChatController.java
@@ -0,0 +1,22 @@
+package com.digital.chat.springback.controllers;
+
+import com.digital.chat.springback.models.documents.Mensaje;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import java.util.Date;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/mensaje")
+    @SendTo("/chat/mensaje")
+    public Mensaje recibirMensaje(Mensaje mensaje){
+        mensaje.setFecha(new Date().getTime());
+        mensaje.setTexto("Recibido por el broker: "+ mensaje.getTexto());
+        return mensaje;
+
+    }
+
+}
