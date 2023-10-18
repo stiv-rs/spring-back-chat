6a0533daf4eb7d8fcf5568fba9f78418aabb8b29
Stiv <strodriguezna@gmail.com>
2023-10-18 11:11:02 +0200

    first commit

---
+++ src/main/java/com/digital/chat/springback/SpringBackChatApplication.java
@@ -0,0 +1,13 @@
+package com.digital.chat.springback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBackChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digital/chat/springback/SpringBackChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital.chat.springback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
